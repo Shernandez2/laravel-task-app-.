c8b2d5b90ec064a17fa95978ecf91ca0e6cc207c
Shernandez2 <142362712+Shernandez2@users.noreply.github.com>
2025-02-20 00:29:34 +0000

    2/19/2025

--- taskApp/routes/web.php
+++ taskApp/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\TaskController;
 Route::get('/', function () {
     return view('tasks.index');
 });
+Route::resource('tasks', TaskController::class);
+Route::redirect('/', '/tasks');
+
