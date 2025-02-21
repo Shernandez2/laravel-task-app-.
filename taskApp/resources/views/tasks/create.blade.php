0b4cf8e10b0bbcfcf7ba7d2a4a2bf2673049041c
Myles Marshall <142362617+MylesMarshall06@users.noreply.github.com>
2025-02-21 23:09:01 +0000

    asdasd

--- taskApp/resources/views/tasks/create.blade.php
+++ taskApp/resources/views/tasks/create.blade.php
@@ -55,7 +55,7 @@
             </div>
 
             <div class="flex gap-4">
-                <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Create Task</button>
+                <button type="submit" class="bg-green-700 text-white px-4 py-2 rounded hover:bg-green-800">Create Task</button>
 
                 <a href="{{ url('/') }}" class="bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600">Cancel</a>
             </div>
